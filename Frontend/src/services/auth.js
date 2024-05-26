322c2f1e17810ba7db6b137c70bff45b02eef2b7
Madhu <114986425+MadhuriGali@users.noreply.github.com>
2024-05-26 22:07:36 +0530

    Update auth.js

--- Frontend/src/services/auth.js
+++ Frontend/src/services/auth.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const signUp=async (name,email,password)=>{
     try{
-        const response=await axios.post("https://todo-h68x093wn-madhus-projects-e4c58fb9.vercel.app/user/signUp",{
+        const response=await axios.post("https://todo-m1cxd9afo-madhus-projects-e4c58fb9.vercel.app/user/signUp",{
         name,
         email,
         password
@@ -18,7 +18,7 @@ export const signUp=async (name,email,password)=>{
 }
 export const login=async (email,password)=>{
     try{
-        const response=await axios.post("https://todo-h68x093wn-madhus-projects-e4c58fb9.vercel.app/user/login",{
+        const response=await axios.post("https://todo-m1cxd9afo-madhus-projects-e4c58fb9.vercel.app/user/login",{
             email,password
         })
        // console.log(response);
